6efd73fc8f6d26ef7eefbcff906241bf3515e77a
James Olsen <olsenjames1116@gmail.com>
2024-05-16 19:32:32 -0400

    Establish session authentication

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'James',
+      username: 'james',
+      password: 'password',
+    },
+    {
+      id: 2,
+      name: 'Kayla',
+      username: 'kayla',
+      password: '123',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
